40b84555ddfc7653cb44af04e8061a2828c04ec5
Fuad Yusuf Awil <85892130+fyawil@users.noreply.github.com>
2023-01-02 23:15:18 +0000

    Updated defined.js with performance factors

--- defined/defined.js
+++ defined/defined.js
@@ -42,7 +42,7 @@ class VoiceOfTheCustomer extends React.Component {
     <div>
       <input type="text" placeholder="What does the customer want?" onChange={this.handleChangePara}></input>
       <button onClick={this.addPara}>Add</button>
-      <Tree rootOfQuality={this.state.parameters} />
+      <Roots rootOfQuality={this.state.parameters} />
     </div>
     )
 }
@@ -50,22 +50,74 @@ class VoiceOfTheCustomer extends React.Component {
 
 }
 
-class Tree extends React.Component {
+class Roots extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+
+    this.state = {
+      elaboration: "",
+      elaborations: []
+    }
+
+    this.handleChangeElabo = this.handleChangeElabo.bind(this);
+    this.addElabo = this.addElabo.bind(this);
   }
+
+  handleChangeElabo(event) {
+    this.setState({
+      elabo: event.target.value
+    })
+  }
+
+  addElabo() {
+    this.setState({
+      elaborations: [...this.state.elaborations, this.state.elabo]
+    })
+  }
+
 
   render(){
     return (
       <div>
-      <h2>{this.props.rootOfQuality[0]}</h2>
-      <h2>{this.props.rootOfQuality[1]}</h2>
-      <h2>{this.props.rootOfQuality[2]}</h2>
-      <h2>{this.props.rootOfQuality[3]}</h2>
+
+      <div>
+        <div>
+          {this.props.rootOfQuality.length >= 1 && <h2>{this.props.rootOfQuality[0]}</h2> }
+          <input type="text" placeholder={`How could '${this.props.rootOfQuality[0]}' be achieved?`} 
+          onChange={this.handleChangeElabo}></input> 
+          <button onClick={this.addElabo}>Add</button>
+        </div>
+
+      {this.props.rootOfQuality.length >= 2 && <h2>{this.props.rootOfQuality[1]}</h2> }
+      {this.props.rootOfQuality.length >+ 3 && <h2>{this.props.rootOfQuality[2]}</h2> }
+      {this.props.rootOfQuality.length >= 4 && <h2>{this.props.rootOfQuality[3]}</h2> }
+      {this.props.rootOfQuality.length >= 5 && <h2>{this.props.rootOfQuality[4]}</h2> }
+      </div>
+
+      <div>
+      <Branches elaborations= {this.state.elaborations}/>
+      </div>
+
       </div>
     )
   }
 }
+
+class Branches extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+
+  render(){
+    return (
+      <div>
+        <h3>{this.props.elaborations}</h3>
+      </div>
+    )
+  }
+}
+
 
 
 
