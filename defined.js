485e5c1c90b2e5d20046cc038762d240a3708402
Fuad Yusuf Awil <85892130+fyawil@users.noreply.github.com>
2023-01-14 12:58:34 +0000

    Redid defined.js and defined.css from scratch to MVP

--- defined.js
+++ defined.js
@@ -1,232 +1,170 @@
-class Defined extends React.Component {
+class Defined extends React.Component{
   constructor(props){
-  super(props);
+    super(props)
 }
 
 render(){
-        return (
-            <div id="main">
-              <h1 id="header">Defined</h1>
-              <Level1Definition/>
-            </div>
-);
-    }
-  }
-
-class Level1Definition extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      level1QualityPara: "",
-      level1QualityParameters: []
-    };
-
-    this.handleChangeLevel1QualityPara = this.handleChangeLevel1QualityPara.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this)
-    this.addLevel1QualityPara = this.addLevel1QualityPara.bind(this);
-  }
-
-  handleChangeLevel1QualityPara(event) {
-    this.setState({
-      level1QualityPara: event.target.value
-    })
-  }
-
-  handleKeyDown(event) {
-    if (event.key == 'Enter') {
-      this.addLevel1QualityPara();
-    }
-  }
-
-  addLevel1QualityPara() {
-    this.setState({
-      level1QualityParameters: [...this.state.level1QualityParameters, this.state.level1QualityPara]
-    })
-  }
-  
-  render(){
-    return (
-    <div id="level1def">
-      <input id="level1definer" type="text" placeholder="What does the customer want?" onChange={this.handleChangeLevel1QualityPara} onKeyDown={this.handleKeyDown}></input>
-      <Level2Definition level1QualityParameters={this.state.level1QualityParameters} level1QualityPara={this.state.level1QualityPara} />
+  return (
+    <div>
+      <h1>Defined</h1>
+      <Level1Definer/>
     </div>
-    )
+  )
+}
 }
 
-
-}
-
-class Level2Definition extends React.Component {
+//This component allows user to input user needs
+class Level1Definer extends React.Component{
   constructor(props){
     super(props);
-
     this.state = {
-      level2QualityPara: "",
-      level2QualityParameters: []
-    }
-
-    this.handleChangeLevel2QualityPara = this.handleChangeLevel2QualityPara.bind(this);
+      //level1def holds what the user inputs as a Level1 parameter
+      level1def: "",
+      //level2paras holds all level1 paras
+      level1paras: []
+    };
+    this.handleChange = this.handleChange.bind(this)
     this.handleKeyDown = this.handleKeyDown.bind(this)
-    this.addlevel2QualityPara = this.addlevel2QualityPara.bind(this);
-  }
+}
 
-  handleChangeLevel2QualityPara(event) {
+// This updates level1def state any time the level1definer input field is changed
+handleChange(event){
+  this.setState({
+    level1def: event.target.value
+  })
+}
+
+// This adds level1def to level1paras if user presses enter and the input is not already included in level1paras or empty
+handleKeyDown(event){
+  if(event.key == "Enter" && !this.state.level1paras.includes(this.state.level1def.trim()) && this.state.level1def != ""){
     this.setState({
-      level2QualityPara: event.target.value
+      level1paras: [...this.state.level1paras, this.state.level1def]
     })
   }
+}
 
-  handleKeyDown(event) {
-    if (event.key == 'Enter') {
-      this.addlevel2QualityPara(event.target.id == "firstLevel1QualityParameterDefiner"?
-                    this.props.level1QualityParameters[0]:
-                    event.target.id == "secondLevel1QualityParameterDefiner"?
-                    this.props.level1QualityParameters[1]:
-                    event.target.id == "thirdLevel1QualityParameterDefiner"?
-                    this.props.level1QualityParameters[2]:
-                    event.target.id == "fourthLevel1QualityParameterDefiner"?
-                    this.props.level1QualityParameters[3]:
-                    event.target.id == "fifthLevel1QualityParameterDefiner"?
-                    this.props.level1QualityParameters[4]:
-                    "Quality parameter does not exist...");  
-      this.setState({
-                level2QualityPara: ""
-                    })
+render(){
+  return (
+    <div id="levels">
+      <div id="level1definer">
+        <input type="text" placeholder="What does the user need?" onChange={this.handleChange} onKeyDown={this.handleKeyDown}></input>
+      </div>
+      {/*level1paras are passed to Level2Definer*/}
+      <Level2Definer id="level1andlevel2andlevel3" level1paras={this.state.level1paras}/>
+    </div>
+  )
+}
+}
+
+//This component allows user to input parameters to further specify level1 user needs
+class Level2Definer extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      level2def: "",
+      level2paras: []
     }
-  }
 
-  addlevel2QualityPara(level1QualityPara) {
-    if(this.state.level2QualityPara != this.state.level2QualityParameters[this.state.level2QualityParameters.length - 1]){
+    this.handleChange = this.handleChange.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+}
+
+// This updates level2def state any time the level2definer input field is changed
+handleChange(event){
+  this.setState({
+    level2def: event.target.value
+  })
+}
+
+// This adds level2def to level2paras if user presses enter and the input is not already included in level2paras or empty
+handleKeyDown(event){
+  if(event.key == "Enter" && !this.state.level2paras.includes(this.state.level2def.trim()) && this.state.level2def != ""){
     this.setState({
-      level2QualityParameters: [...this.state.level2QualityParameters, [level1QualityPara, this.state.level2QualityPara]]
-    })      
+      level2paras: [...this.state.level2paras, this.state.level2def]
+    })
+  }
+}
+
+render(){
+
+  return (
+    <div id="level1andlevel2andlevel3">
+      <div id="level1paras">
+        {this.props.level1paras
+      .map(
+        para => 
+        <React.Fragment>
+          <h2>{para}</h2>
+          <input type="text" onChange={this.handleChange} onKeyDown={this.handleKeyDown}></input>
+        </React.Fragment>     
+      )}
+      </div>
+      <Level3Definer id="level2andlevel3" level2paras={this.state.level2paras} />
+    </div>
+  )
+}
+}
+
+//The Level3 component allows user to input parameters to measure level3 quality parameters objectively
+class Level3Definer extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      level3def: "",
+      level3paras: []
     }
 
-  }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+}
 
+// This updates level3def state any time the level3definer input field is changed
+handleChange(event){
+  this.setState({
+    level3def: event.target.value
+  })
+}
 
-  render(){
-    return (
-      <div id="level2def">
-
-      <div>
-        <div>
-          {this.props.level1QualityParameters.length >= 1 && 
-          <div>
-            <h2 id="firstLevel1QualityParameter">{this.props.level1QualityParameters[0]}</h2> 
-            <input type="text" id="firstLevel1QualityParameterDefiner" 
-            placeholder={`How could '${this.props.level1QualityParameters[0]}' be achieved?`} 
-            onChange={this.handleChangeLevel2QualityPara} onKeyDown={this.handleKeyDown}></input> 
-          </div>
-          }
-        </div>
-        <div>
-          {this.props.level1QualityParameters.length >= 2 && 
-          <div>
-            <h2 id="secondLevel1QualityParameter">{this.props.level1QualityParameters[1]}</h2> 
-            <input type="text" id="secondLevel1QualityParameterDefiner" 
-            placeholder={`How could '${this.props.level1QualityParameters[1]}' be achieved?`} 
-            onChange={this.handleChangeLevel2QualityPara} onKeyDown={this.handleKeyDown}></input> 
-          </div>
-          }
-        </div>
-        <div>
-          {this.props.level1QualityParameters.length >= 3 && 
-          <div>
-            <h2 id="thirdLevel1QualityParameter">{this.props.level1QualityParameters[2]}</h2> 
-            <input type="text" id="thirdLevel1QualityParameterDefiner" 
-            placeholder={`How could '${this.props.level1QualityParameters[2]}' be achieved?`} 
-            onChange={this.handleChangeLevel2QualityPara} onKeyDown={this.handleKeyDown}></input> 
-          </div>
-          }
-        </div>
-        <div>
-          {this.props.level1QualityParameters.length >= 4 && 
-          <div>
-            <h2 id="fourthLevel1QualityParameter">{this.props.level1QualityParameters[3]}</h2> 
-            <input type="text" id="fourthLevel1QualityParameterDefiner"
-            placeholder={`How could '${this.props.level1QualityParameters[3]}' be achieved?`} 
-            onChange={this.handleChangeLevel2QualityPara} onKeyDown={this.handleKeyDown}></input> 
-          </div>
-          }
-        </div>
-        <div>
-          {this.props.level1QualityParameters.length >= 5 && 
-          <div>
-            <h2 id="fifthLevel1QualityParameter">{this.props.level1QualityParameters[4]}</h2> 
-            <input type="text" id="fifthLevel1QualityParameterDefiner"
-            placeholder={`How could '${this.props.level1QualityParameters[4]}' be achieved?`} 
-            onChange={this.handleChangeLevel2QualityPara} onKeyDown={this.handleKeyDown}></input> 
-          </div>
-          }
-        </div>
-      </div>
-
-      <div>
-      <Level2Parameters level2QualityParameters= {this.state.level2QualityParameters} level1QualityParameters= {this.props.level1QualityParameters}/>
-      </div>
-
-      </div>
-    )
+// This adds level2def to level2paras if user presses enter and the input is not already included in level2paras or empty
+handleKeyDown(event){
+  if(event.key == "Enter" && !this.state.level3paras.includes(this.state.level3def.trim()) && this.state.level3def != ""){
+    this.setState({
+      level3paras: [...this.state.level3paras, this.state.level3def]
+    })
   }
 }
 
-class Level2Parameters extends React.Component {
-  constructor(props){
-    super(props)
-  }
-
-
-  render(){
-
-    let firstLevel1QualityParametersLevel2Parameters = [...this.props.level2QualityParameters]
-                            .filter(level2QualityPara => level2QualityPara[0] == this.props.level1QualityParameters[0])
-                            .map(level2QualityPara => level2QualityPara[1])
-                            .join(" ")
-    let secondLevel2QualityParametersLevel2Parameters = [...this.props.level2QualityParameters]
-                            .filter(level2QualityPara => level2QualityPara[0] == this.props.level1QualityParameters[1])
-                            .map(level2QualityPara => level2QualityPara[1])
-                            .join(" ")
-    let thirdLevel1QualityParametersLevel2Parameters = [...this.props.level2QualityParameters]
-                            .filter(level2QualityPara => level2QualityPara[0] == this.props.level1QualityParameters[2])
-                            .map(level2QualityPara => level2QualityPara[1])
-                            .join(" ");
-    let fourthLevel1QualityParametersLevel2Parameters = [...this.props.level2QualityParameters]
-                            .filter(level2QualityPara => level2QualityPara[0] == this.props.level1QualityParameters[3])
-                            .map(level2QualityPara => level2QualityPara[1])
-                            .join(" ")
-    let fifthLevel1QualityParametersLevel2Parameters = [...this.props.level2QualityParameters]
-                            .filter(level2QualityPara => level2QualityPara[0] == this.props.level1QualityParameters[4])
-                            .map(level2QualityPara => level2QualityPara[1])
-                            .join(" ")
-
-    return (
+render(){
+  return (
+    <div id="level2andlevel3">
       <div id="level2paras">
-        <h3 id="firstLevel1QualityParametersLevel2Parameters">{firstLevel1QualityParametersLevel2Parameters}</h3>
-        <h3 id="secondLevel2QualityParametersLevel2Parameters">{secondLevel2QualityParametersLevel2Parameters}</h3>
-        <h3 id="thirdLevel1QualityParametersLevel2Parameters">{thirdLevel1QualityParametersLevel2Parameters}</h3>
-        <h3 id="fourthLevel1QualityParametersLevel2Parameters">{fourthLevel1QualityParametersLevel2Parameters}</h3>
-        <h3 id="fifthLevel1QualityParametersLevel2Parameters">{fifthLevel1QualityParametersLevel2Parameters}</h3>
-        <Level3Parameters/>
+        {this.props.level2paras
+      .map(
+        para => 
+        <React.Fragment>
+          <h2>{para}</h2>
+          <input type="text" onChange={this.handleChange} onKeyDown={this.handleKeyDown}></input>
+        </React.Fragment>     
+      )}
       </div>
-    )
-  }
+      <Level3Parameters id="level3paras" level3paras={this.state.level3paras} />
+    </div>
+  )
+}
 }
 
-class Level3Parameters extends React.Component {
+class Level3Parameters extends React.Component{
   constructor(props){
     super(props)
-  }
-  render(){
-    return (
-      <div id="level3paras">
-      </div>
-    )
-  }
 }
 
-
-
-
+render(){
+  return (
+    <div>
+      {this.props.level3paras.map(para => <React.Fragment><h2>{para}</h2></React.Fragment>)}
+    </div>
+  )
+}
+}
 
 ReactDOM.render(<Defined />, document.getElementById('defined'));
