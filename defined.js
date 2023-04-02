c8d343af881ae56899b7873a1f3c008c2cb3ad67
Fuad Yusuf Awil <85892130+fyawil@users.noreply.github.com>
2023-04-02 14:51:33 +0100

    Debug defned.js

--- defined.js
+++ defined.js
@@ -41,11 +41,6 @@ handleKeyDown(event){
       level1paras: [...this.state.level1paras, this.state.level1def]
     })
   }
-  if(event.key == "Next" && !this.state.level1paras.includes(this.state.level1def.trim()) && this.state.level1def != ""){
-    this.setState({
-      level1paras: [...this.state.level1paras, this.state.level1def]
-    })
-  }
 }
 
 render(){
@@ -88,7 +83,7 @@ handleKeyDown(event){
       level2paras: [...this.state.level2paras, this.state.level2def]
     })
   }
-  if(event.key == "Next" && !this.state.level2paras.includes(this.state.level2def.trim()) && this.state.level2def != ""){
+  if(event.key == "Tab" && !this.state.level2paras.includes(this.state.level2def.trim()) && this.state.level2def != ""){
     this.setState({
       level2paras: [...this.state.level2paras, this.state.level2def]
     })
@@ -140,11 +135,6 @@ handleChange(event){
 // This adds level2def to level2paras if user presses enter and the input is not already included in level2paras or empty
 handleKeyDown(event){
   if(event.key == "Enter" && !this.state.level3paras.includes(this.state.level3def.trim()) && this.state.level3def != ""){
-    this.setState({
-      level3paras: [...this.state.level3paras, this.state.level3def]
-    })
-  }
-  if(event.key == "Next" && !this.state.level3paras.includes(this.state.level3def.trim()) && this.state.level3def != ""){
     this.setState({
       level3paras: [...this.state.level3paras, this.state.level3def]
     })
